6798a5ef0f57642b3d88c7021bdbebb4dafb3037
C0DEFLUX <niksklavins04@gmail.com>
2024-11-25 18:43:52 +0200

    Added missing class

--- TicketSys/src/AddTicketPrices.java
+++ TicketSys/src/AddTicketPrices.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddTicketPrices
 {
+    static ArrayList<Double> ticketPrices = new ArrayList<>();
 
+    public static void  addTicket(Scanner scanner)
+    {
+        System.out.println("Enter ticket price: ");
+        double ticketPrice = scanner.nextDouble();
+
+        if(ticketPrice > 0)
+        {
+            ticketPrices.add(ticketPrice);
+            System.out.println("Ticket added!");
+        }
+
+    }
 }
