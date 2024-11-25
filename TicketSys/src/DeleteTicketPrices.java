d4ee98f8a1333d319987d8bb40f21d9beec0475d
C0DEFLUX <niksklavins04@gmail.com>
2024-11-25 19:01:43 +0200

    Added delete class

--- TicketSys/src/DeleteTicketPrices.java
+++ TicketSys/src/DeleteTicketPrices.java
@@ -1,4 +1,20 @@
-public class DeleteTicketPrices
-{
+import java.util.Scanner;
 
+public class DeleteTicketPrices extends AddTicketPrices
+{
+    public static void deleteTicket(Scanner scanner)
+    {
+        ListTicketPrices.listTicketPrices();
+        System.out.println("Enter the index of ticket you want to delete");
+        int index = scanner.nextInt();
+
+        if(index >= 0 && index < ticketPrices.size())
+        {
+            ticketPrices.remove(index);
+            System.out.println("Ticket deleted successfully");
+            return;
+        }
+
+        System.out.println("Invalid index");
+    }
 }
