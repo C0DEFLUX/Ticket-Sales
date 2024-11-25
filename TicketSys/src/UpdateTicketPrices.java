091a501489c8ead3e669f76b0c0d323492cf150c
C0DEFLUX <niksklavins04@gmail.com>
2024-11-25 18:57:25 +0200

    Finished update ticket class

--- TicketSys/src/UpdateTicketPrices.java
+++ TicketSys/src/UpdateTicketPrices.java
@@ -12,6 +12,18 @@ public class UpdateTicketPrices extends AddTicketPrices
         if(index >= 0 && index < ticketPrices.size())
         {
             System.out.print("Enter new ticket price: ");
+            double newPrice = scanner.nextDouble();
+
+            if(newPrice > 0)
+            {
+                ticketPrices.set(index, newPrice);
+                System.out.println("Ticket price updated successfully. New price: " + newPrice);
+                return;
+            }
+
+            System.out.println("Price must be greater than 0.");
+            return;
         }
+        System.out.println("Invalid index!");
     }
 }
