cc2e5ed59eaced37f1a9751ccaa742a55e8b2e9d
C0DEFLUX <niksklavins04@gmail.com>
2024-11-25 19:24:18 +0200

    More comments

--- TicketSys/src/AddTicketPrices.java
+++ TicketSys/src/AddTicketPrices.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 
 public class AddTicketPrices
 {
+    //make an array list
     static ArrayList<Double> ticketPrices = new ArrayList<>();
 
     public static void  addTicket(Scanner scanner)
     {
+        //add ticket prices to array
         System.out.println("Enter ticket price: ");
         double ticketPrice = scanner.nextDouble();
 
+        //check if ticket prices are > 0
         if(ticketPrice > 0)
         {
             ticketPrices.add(ticketPrice);
